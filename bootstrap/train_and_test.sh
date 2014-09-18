7e6ae65488d33334268c253fee3fa9d8d2482073
Dylan Hadfield-Menell <dhadfieldmenell@gmail.com>
2014-09-18 16:39:48 -0700

    adding some small changes

--- bootstrap/train_and_test.sh
+++ bootstrap/train_and_test.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-python bootstrap/bootstrapping.py --train_sizes 10 30 --alpha 100 derived-traj
-python bootstrap/eval.py --train_sizes 10 30 --i_end 10 eval bigdata/bootstrap/bootstrap_res.h5 bigdata/misc/sept_13_0.1_test.h5 bij
+python bootstrap/bootstrapping.py --train_sizes 50 100 --alpha 100 derived-traj bigdata/bootstrap/bootstrap_res_0.15_0.h5 bigdata/bootstrap/sept_13_0.15_train_0.h5 
+python bootstrap/eval.py --train_sizes 50 100 --i_end 100 eval bigdata/bootstrap/bootstrap_res_0.15_0.h5 bigdata/misc/sept_13_0.15_test.h5 bij
