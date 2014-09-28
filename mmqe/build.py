38a0e040b80a6069a993cd8847d466b83563cb5c
Chelsea Finn <cbfinn@eecs.berkeley.edu>
2014-09-28 00:27:34 -0700

    change directory and failure mode constraints

--- mmqe/build.py
+++ mmqe/build.py
@@ -85,8 +85,10 @@ def build_constraints(args):
     if not check_exists(args.constrfile):
         return
     constrfile = h5py.File(args.constrfile, 'w')
-    n_constraints = len(exp_demofile)        
+    n_constraints = len([k for k in exp_demofile.keys() if k.startswith('(')])        
     for i, demo_k in enumerate(exp_demofile):
+        if i.startswith('f'):
+            continue
         ## we expect states to be an identifier and a 
         ## point cloud, we won't use the identifier here
         sys.stdout.write('\rcomputing constraints {}/{}       '.format(i, n_constraints))
