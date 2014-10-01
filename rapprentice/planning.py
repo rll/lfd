15bb2b8c0ee17f6601bf3688d57a69c6a2e548e2
Christopher Lin <c.l@berkeley.edu>
2014-10-01 16:29:34 -0700

    added old follow_traj to see if hybridPlanner with execute better trajectories

--- rapprentice/planning.py
+++ rapprentice/planning.py
@@ -6,7 +6,78 @@ from rapprentice import tps, registration, math_utils as mu
 from rapprentice.registration import ThinPlateSpline
 import IPython as ipy
 
-def plan_follow_traj(robot, manip_name, ee_link, new_hmats, old_traj, 
+def plan_follow_traj(robot, manip_name, ee_link, new_hmats, old_traj):
+        
+    n_steps = len(new_hmats)
+    assert old_traj.shape[0] == n_steps
+    assert old_traj.shape[1] == 7
+    
+    arm_inds  = robot.GetManipulator(manip_name).GetArmIndices()
+
+    ee_linkname = ee_link.GetName()
+    
+    init_traj = old_traj.copy()
+    #init_traj[0] = robot.GetDOFValues(arm_inds)
+
+    request = {
+        "basic_info" : {
+            "n_steps" : n_steps,
+            "manip" : manip_name,
+            "start_fixed" : False
+        },
+        "costs" : [
+        {
+            "type" : "joint_vel",
+            "params": {"coeffs" : [n_steps/5.]}
+        },
+        {
+            "type" : "collision",
+            "params" : {"coeffs" : [10],"dist_pen" : [0.01]}
+        }                
+        ],
+        "constraints" : [
+        ],
+        "init_info" : {
+            "type":"given_traj",
+            "data":[x.tolist() for x in init_traj]
+        }
+    }
+        
+    poses = [openravepy.poseFromMatrix(hmat) for hmat in new_hmats]
+    for (i_step,pose) in enumerate(poses):
+        request["costs"].append(
+            {"type":"pose",
+             "params":{
+                "xyz":pose[4:7].tolist(),
+                "wxyz":pose[0:4].tolist(),
+                "link":ee_linkname,
+                "timestep":i_step,
+                "pos_coeffs":[10,10,10],
+                "rot_coeffs":[10,10,10]
+             }
+            })
+
+    s = json.dumps(request)
+    prob = trajoptpy.ConstructProblem(s, robot.GetEnv()) # create object that stores optimization problem
+    result = trajoptpy.OptimizeProblem(prob) # do optimization
+    traj = result.GetTraj()    
+        
+    saver = openravepy.RobotStateSaver(robot)
+    pos_errs = []
+    for i_step in xrange(1,n_steps):
+        row = traj[i_step]
+        robot.SetDOFValues(row, arm_inds)
+        tf = ee_link.GetTransform()
+        pos = tf[:3,3]
+        pos_err = np.linalg.norm(poses[i_step][4:7] - pos)
+        pos_errs.append(pos_err)
+    pos_errs = np.array(pos_errs)
+        
+    print "planned trajectory for %s. max position error: %.3f. all position errors: %s"%(manip_name, pos_errs.max(), pos_errs)
+            
+    return traj
+
+def plan_follow_traj2(robot, manip_name, ee_link, new_hmats, old_traj, 
                      no_collision_cost_first=False, use_collision_cost=True, start_fixed=False, joint_vel_limits=None,
                      beta_pos = 1000.0, beta_rot = 10.0, gamma = 1000.0):
     orig_dof_inds = robot.GetActiveDOFIndices()
