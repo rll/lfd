941153d32f3a524aa9a2ff4fb35406b42cb0777d
Chelsea Finn <cbfinn@berkeley.edu>
2014-10-02 14:26:43 -0700

    bash script to build all figure 8

---
+++ scripts/build_all.sh
@@ -0,0 +1,36 @@
+
+python build.py mul_grip bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_fig8_sampledlabel_40a.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+
+#python build.py mul_s bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_sampledlabel_40b.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+python build.py mul_grip bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_fig8_sampledlabel_40b.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+
+#python build.py mul_s bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_sampledlabel_40c.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+python build.py mul_grip bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_fig8_sampledlabel_40c.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+
+
+#python build.py mul_s bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_sampledlabel_80a.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+python build.py mul_grip bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_fig8_sampledlabel_80a.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+
+
+#python build.py mul_s bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_sampledlabel_80b.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+python build.py mul_grip bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_fig8_sampledlabel_80b.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+
+#python build.py mul_s bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_sampledlabel_80c.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+python build.py mul_grip bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_fig8_sampledlabel_80c.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+
+#python build.py mul_s bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_sampledlabel_120a.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+python build.py mul_grip bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_fig8_sampledlabel_120a.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+
+#python build.py mul_s bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_sampledlabel_120b.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+python build.py mul_grip bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_fig8_sampledlabel_120b.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+
+
+#python build.py mul_s bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_sampledlabel_120c.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+python build.py mul_grip bellman ../data/misc/Oct1_landmarks.h5 full ../data/labels/Oct1_fig8_sampledlabel_120c.h5 ../bigdata/misc/overhand_actions.h5 --C 100
+
+
+
+
+
+
+
