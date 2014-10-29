b310de2f80db76aa15179cbbf6f63fb6da2674b6
Alex Lee <alexleegk@gmail.com>
2014-10-28 21:32:01 -0700

    Choose better default constants

--- registration/constants.py
+++ registration/constants.py
@@ -2,9 +2,9 @@ import numpy as np
 
 class TpsConstant(object):
     N_ITER        = 20
-    EM_ITER       = 2
-    REG           = (10, .1)
-    RAD           = (.04, .00004)
+    EM_ITER       = 1
+    REG           = (.1, .0001)
+    RAD           = (.01, .0001)
     ROT_REG       = np.r_[1e-4, 1e-4, 1e-1]
     OUTLIER_PRIOR = .1
     OURLIER_FRAC  = 1e-2
