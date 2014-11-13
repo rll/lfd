24ca439a93848b5d9347ba354b52793741544b3f
Alex Lee <alexleegk@gmail.com>
2014-11-12 17:11:44 -0800

    Fix rapprentice import

--- lfd/registration/plotting_openrave.py
+++ lfd/registration/plotting_openrave.py
@@ -1,6 +1,6 @@
 from __future__ import division
 
-import rapprentice
+from lfd.rapprentice import plotting_openrave
 
 # TODO: rapprentice.plotting_openrave and other openrave plottings should go in this file
 
@@ -10,6 +10,6 @@ def registration_plot_cb(sim, x_nd, y_md, xtarg_nd, corr_nm, wt_n, f):
     handles.append(sim.env.plot3(y_md, 5, (0,0,1)))
     xwarped_nd = f.transform_points(x_nd)
     handles.append(sim.env.plot3(xwarped_nd, 5, (0,1,0)))
-    handles.extend(rapprentice.plotting_openrave.draw_grid(sim.env, f.transform_points, x_nd.min(axis=0), x_nd.max(axis=0), xres = .1, yres = .1, zres = .04))
+    handles.extend(plotting_openrave.draw_grid(sim.env, f.transform_points, x_nd.min(axis=0), x_nd.max(axis=0), xres = .1, yres = .1, zres = .04))
     if sim.viewer:
         sim.viewer.Step()
