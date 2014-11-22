e7e6feaf57630746cd92352d8e25937481bb0f4a
Alex Lee <alexleegk@gmail.com>
2014-11-22 14:42:52 -0800

    Add docstring comments in settings.py files so they get generated in doc.

--- lfd/environment/settings.py
+++ lfd/environment/settings.py
@@ -1,15 +1,26 @@
+#:
 GRIPPER_OPEN_CLOSE_THRESH = 0.04 # .06 for fig8 (thick rope), 0.04 for thin rope (overhand)
 
+#:
 ROPE_RADIUS        = .005
+#:
 ROPE_ANG_STIFFNESS = .1
+#:
 ROPE_ANG_DAMPING   = 1
+#:
 ROPE_LIN_DAMPING   = .75
+#:
 ROPE_ANG_LIMIT     = .4
+#:
 ROPE_LIN_STOP_ERP  = .2
+#:
 ROPE_MASS          = 1.0
+#:
 ROPE_RADIUS_THICK  = .008
 
+#: window properties for the viewer's window
 WINDOW_PROP = [0,0,1500,1500]
+#: transposed homogeneous matrix for the viewer's camera
 CAMERA_MATRIX = [[    0, 1,   0, 0],
 				 [   -1, 0, 0.5, 0],
 				 [  0.5, 0,   1, 0],
