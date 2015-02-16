64b8be9b96dacfeec8316502a3af6490fc9b5936
Alex Lee <alexleegk@gmail.com>
2015-02-16 13:12:10 -0800

    Decouple source points from control points in tps_hc2.

--- lfd/registration/hc.py
+++ lfd/registration/hc.py
@@ -22,132 +22,135 @@ def gradymin(x, y, beta=None):
     #     grad = exp_m_beta_x / (exp_m_beta_x + np.exp(-beta * y))
     return (x <= y).astype(float)
 
-def int_p2(x_nd, y_md):
-    n, _ = x_nd.shape
+def int_p2(x_ld, y_md):
+    l, _ = x_ld.shape
     m, _ = y_md.shape
     # v = 0
-    # for i in range(n):
+    # for i in range(l):
     #     for j in range(m):
-    #         v += min(x_nd[i,0], y_md[j,0]) * min(x_nd[i,1], y_md[j,1]) #* min(x_nd[i,2], y_md[j,2])
-    # v /= n * m
+    #         v += min(x_ld[i,0], y_md[j,0]) * min(x_ld[i,1], y_md[j,1]) #* min(x_ld[i,2], y_md[j,2])
+    # v /= l * m
 
-    x_ndm = np.tile(x_nd[:,:,None], (1,1,m))
-    v = np.sum(np.prod(np.minimum(x_ndm, y_md.T), axis=1)) / (n * m)
+    x_ldm = np.tile(x_ld[:,:,None], (1,1,m))
+    v = np.sum(np.prod(np.minimum(x_ldm, y_md.T), axis=1)) / (l * m)
     return v
 
-def grad_p2(x_nd, y_md=None):
-    """If y_md is passed, it must not be the same as x_nd."""
+def grad_p2(x_ld, y_md=None):
+    """If y_md is passed, it must not be the same as x_ld."""
     if y_md is None:
-        y_md = x_nd
-        n, d = x_nd.shape
+        y_md = x_ld
+        l, d = x_ld.shape
         m, _ = y_md.shape
         # # naive, d == 2
-        # gv_nd = np.zeros((n, d))
-        # for i in range(n):
+        # gv_ld = np.zeros((l, d))
+        # for i in range(l):
         #     for j in range(m):
         #         if i != j:
-        #             gv_nd[i,0] += 2 * gradymin(x_nd[i,0], y_md[j,0]) * min(x_nd[i,1], y_md[j,1])
-        #             gv_nd[i,1] += 2 * min(x_nd[i,0], y_md[j,0]) * gradymin(x_nd[i,1], y_md[j,1])
+        #             gv_ld[i,0] += 2 * gradymin(x_ld[i,0], y_md[j,0]) * min(x_ld[i,1], y_md[j,1])
+        #             gv_ld[i,1] += 2 * min(x_ld[i,0], y_md[j,0]) * gradymin(x_ld[i,1], y_md[j,1])
         #         else:
-        #             gv_nd[i,0] += min(x_nd[i,1], y_md[j,1])
-        #             gv_nd[i,1] += min(x_nd[i,0], y_md[j,0])
-        # gv_nd /= n * m
+        #             gv_ld[i,0] += min(x_ld[i,1], y_md[j,1])
+        #             gv_ld[i,1] += min(x_ld[i,0], y_md[j,0])
+        # gv_ld /= l * m
 
-        x_ndm = np.tile(x_nd[:,:,None], (1,1,m))
-        gradymin_ndm = gradymin(x_ndm, y_md.T)
-        min_ndm = np.minimum(x_ndm, y_md.T)
-        gv_nd = np.empty((n, d))
+        x_ldm = np.tile(x_ld[:,:,None], (1,1,m))
+        gradymin_ldm = gradymin(x_ldm, y_md.T)
+        min_ldm = np.minimum(x_ldm, y_md.T)
+        gv_ld = np.empty((l, d))
         for i in range(d):
             not_i = np.ones(d, dtype=bool)
             not_i[i] = False
-            c_nm = 2 * gradymin_ndm[:,i,:]
+            c_nm = 2 * gradymin_ldm[:,i,:]
             np.fill_diagonal(c_nm, 1)
-            gv_nd[:,i] = np.sum(c_nm * np.prod(min_ndm[:,not_i,:], axis=1), axis=1)
-        gv_nd /= n * m
+            gv_ld[:,i] = np.sum(c_nm * np.prod(min_ldm[:,not_i,:], axis=1), axis=1)
+        gv_ld /= l * m
     else:
-        n, d = x_nd.shape
+        l, d = x_ld.shape
         m, _ = y_md.shape
         # # naive, d == 2
-        # gv_nd = np.zeros((n, d))
-        # for i in range(n):
+        # gv_ld = np.zeros((l, d))
+        # for i in range(l):
         #     for j in range(m):
-        #         gv_nd[i,0] += gradymin(x_nd[i,0], y_md[j,0]) * min(x_nd[i,1], y_md[j,1])
-        #         gv_nd[i,1] += min(x_nd[i,0], y_md[j,0]) * gradymin(x_nd[i,1], y_md[j,1])
-        # gv_nd /= n * m
+        #         gv_ld[i,0] += gradymin(x_ld[i,0], y_md[j,0]) * min(x_ld[i,1], y_md[j,1])
+        #         gv_ld[i,1] += min(x_ld[i,0], y_md[j,0]) * gradymin(x_ld[i,1], y_md[j,1])
+        # gv_ld /= l * m
 
-        x_ndm = np.tile(x_nd[:,:,None], (1,1,m))
-        gradymin_ndm = gradymin(x_ndm, y_md.T)
-        min_ndm = np.minimum(x_ndm, y_md.T)
-        gv_nd = np.empty((n, d))
+        x_ldm = np.tile(x_ld[:,:,None], (1,1,m))
+        gradymin_ldm = gradymin(x_ldm, y_md.T)
+        min_ldm = np.minimum(x_ldm, y_md.T)
+        gv_ld = np.empty((l, d))
         for i in range(d):
             not_i = np.ones(d, dtype=bool)
             not_i[i] = False
-            gv_nd[:,i] = np.sum(gradymin_ndm[:,i,:] * np.prod(min_ndm[:,not_i,:], axis=1), axis=1)
-        gv_nd /= n * m
-    return gv_nd
+            gv_ld[:,i] = np.sum(gradymin_ldm[:,i,:] * np.prod(min_ldm[:,not_i,:], axis=1), axis=1)
+        gv_ld /= l * m
+    return gv_ld
 
-def hc2_obj(x_knd):
-    k = len(x_knd)
+def hc2_obj(x_kld):
+    k = len(x_kld)
     alpha = 1/k
 
     energy1 = 0
     energy2 = 0
-    for x_nd in x_knd:
-        n , _ = x_nd.shape
-        energy2 += alpha * int_p2(x_nd, x_nd)
-    for x_nd in x_knd:
-        for y_md in x_knd:
-            energy1 += (alpha**2) * int_p2(x_nd, y_md)
+    for x_ld in x_kld:
+        l , _ = x_ld.shape
+        energy2 += alpha * int_p2(x_ld, x_ld)
+    for x_ld in x_kld:
+        for y_md in x_kld:
+            energy1 += (alpha**2) * int_p2(x_ld, y_md)
     energy = -energy1 + energy2
 
-    grads = []
-    for l, x_nd in enumerate(x_knd):
-        n, d = x_nd.shape
-        grad_tmp = np.zeros((n, d))
-        for s, y_md in enumerate(x_knd):
-            if l != s:
+    grad_kld = []
+    for r, x_ld in enumerate(x_kld):
+        l, d = x_ld.shape
+        grad_ld = np.zeros((l, d))
+        for s, y_md in enumerate(x_kld):
+            if r != s:
                 # TODO: why 2?
-                grad_tmp += 2 * (alpha**2) * grad_p2(x_nd, y_md)
-        grad = -grad_tmp + (alpha - alpha**2) * grad_p2(x_nd)
-        grads.append(grad)
-    return energy, grads
+                grad_ld -= 2 * (alpha**2) * grad_p2(x_ld, y_md)
+        grad_ld += (alpha - alpha**2) * grad_p2(x_ld)
+        grad_kld.append(grad_ld)
+    return energy, grad_kld
 
-def translate_to_R_plus(x_knd, region):
-    min_x_knd = np.min([np.min(x_nd) for x_nd in x_knd])
-    for x_nd in x_knd:
-        x_nd -= min_x_knd - region
-    return x_knd
+def translate_to_R_plus(x_kld, region):
+    min_x_kld = np.min([np.min(x_ld) for x_ld in x_kld])
+    for x_ld in x_kld:
+        x_ld -= min_x_kld - region
+    return x_kld
 
 def tps_hc2_obj(z_knd, f_k, reg, rot_reg):
-    xwarped_knd = []
+    xwarped_kld = []
     i = 0
     for f in f_k:
         n, d = f.z_ng.shape
         f.z_ng = z_knd[i*d:(i+n)*d]
-        xwarped_knd.append(f.transform_points())
+        xwarped_kld.append(f.transform_points())
         i += n
-    xwarped_knd = translate_to_R_plus(xwarped_knd, np.ones(d))
+    xwarped_kld = translate_to_R_plus(xwarped_kld, np.ones(d))
 
-    hc2_energy, hc2_grad_knd = hc2_obj(xwarped_knd)
+    hc2_energy, hc2_grad_kld = hc2_obj(xwarped_kld)
     energy = hc2_energy
     grad_knd = []
-    for f, hc2_grad_nd in zip(f_k, hc2_grad_knd):
+    for f, hc2_grad_ld in zip(f_k, hc2_grad_kld):
         n, d = f.z_ng.shape
         NR_nd = f.N_bn[1:1+d, :].T * rot_reg[:d]
         NRN_nn = NR_nd.dot(f.N_bn[1:1+d, :])
         energy += np.trace(f.z_ng.T.dot(reg * f.NKN_nn + NRN_nn).dot(f.z_ng)) - 2 * np.trace(f.z_ng.T.dot(NR_nd))
-        grad_nd = f.QN_ln.T.dot(hc2_grad_nd)
+        grad_nd = f.QN_ln.T.dot(hc2_grad_ld)
         grad_nd += 2 * (reg * f.NKN_nn + NRN_nn).dot(f.z_ng) - 2 * NR_nd
         grad_knd.append(grad_nd.reshape(n*d))
     grad_knd = np.concatenate(grad_knd)
     return energy, grad_knd
 
-def tps_hc2(x_knd, opt_iter=100, reg=settings.REG[1], rot_reg=settings.ROT_REG, callback=None):
+def tps_hc2(x_kld, ctrl_knd=None, opt_iter=100, reg=settings.REG[1], rot_reg=settings.ROT_REG, callback=None):
+    if ctrl_knd is None:
+        ctrl_knd = x_kld
+
     f_k = []
     z_knd = []
-    for x_nd in x_knd:
-        n, d = x_nd.shape
-        f = ThinPlateSpline(x_nd, x_nd)
+    for x_ld, ctrl_nd in zip(x_kld, ctrl_knd):
+        n, d = ctrl_nd.shape
+        f = ThinPlateSpline(x_ld, ctrl_nd)
         f_k.append(f)
         z_knd.append(f.z_ng.reshape(n*d))
     z_knd = np.concatenate(z_knd)
