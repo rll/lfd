db80b10544fd40de1f0a7a4f28fd8ede1e53572f
jennifershih <jennifershih@Johnbishek.(none)>
2015-05-03 22:14:24 -0700

    trajopt call works for custom robot

--- examples/test_box_trajopt.py
+++ examples/test_box_trajopt.py
@@ -25,12 +25,40 @@ def color_robot(cyl_sim_objs, color=[1, 0, 0]):
             for link in bt_obj.GetKinBody().GetLinks():
                 for geom in link.GetGeometries():
                     geom.SetDiffuseColor(color)
+
+def get_target_pose(env, robot, go_through_hole=False):
+    rave_robot = env.sim.env.GetRobots()[0]
+    robot_T = rave_robot.GetTransform()
+    x, y, z, _ = robot_T[:,3]
+    r = abs(0 - x)
+    
+    target_x = 0
+    target_y = y - r
+    
+    if go_through_hole:
+        # import pdb; pdb.set_trace()
+        target_y = y - 0.15 * 2
+
+    target_pose = openravepy.matrixFromAxisAngle([0, 0, np.pi/2])
+    target_pose[0, 3] = target_x
+    target_pose[1, 3] = target_y
+    target_pose[2, 3] = z
+
+    # rave_robot.SetTransform(target_pose)
+    # env.sim.viewer.Step()
+    # raw_input("! look at target")
+    # import pdb; pdb.set_trace()
+
+    return target_pose
+    
     
 def main():
     # define simulation objects
     sim_objs = []
-    table_width = 0.25 #0.85
-    table_thickness = 0.05
+    # table_width = 0.25 #0.85
+    # table_thickness = 0.05
+    table_width = 0.25
+    table_thickness = 0
     table_x = 0
     table_y = 0
     table_z = 0
@@ -42,14 +70,16 @@ def main():
     obstruction1_height = 0.03
     obstruction1_x = obstruction1_length + hole_size / 2
     obstruction1_y = 0
-    obstruction1_z = table_thickness + obstruction1_height
+    # obstruction1_z = table_thickness + obstruction1_height
+    obstruction1_z = 0
 
     obstruction2_length = obstruction1_length
     obstruction2_width = 0.01
     obstruction2_height = obstruction1_height
     obstruction2_x = -(obstruction2_length + hole_size / 2)
     obstruction2_y = 0
-    obstruction2_z = table_thickness + obstruction2_height
+    # obstruction2_z = table_thickness + obstruction2_height
+    obstruction2_z = 0
 
     # on the bottom right corner. a 2D robot with three dimensions of freedom
     robot_length = 0.030
@@ -57,11 +87,13 @@ def main():
     robot_height = 0.008
     robot_x = table_x + table_width * 0.50
     robot_y = table_y + table_width * 0.75 + robot_width 
-    robot_z = table_z + table_thickness + robot_height
+    # robot_z = table_z + table_thickness + robot_height
+    # robot_z = table_z + table_thickness + robot_height
+    robot_z = 0
 
     k = 10
 
-    sim_objs.append(BoxSimulationObject("table", k*[table_x, table_y, table_z], k*[table_width, table_width, table_thickness], dynamic=False))
+    # sim_objs.append(BoxSimulationObject("table", k*[table_x, table_y, table_z], k*[table_width, table_width, table_thickness], dynamic=False))
     sim_objs.append(BoxSimulationObject("obstruction1", k*[obstruction1_x, obstruction1_y, obstruction1_z], k*[obstruction1_length, obstruction1_width, obstruction1_height], dynamic=False)) 
     sim_objs.append(BoxSimulationObject("obstruction2", k*[obstruction2_x, obstruction2_y, obstruction2_z], k*[obstruction2_length, obstruction2_width, obstruction2_height], dynamic=False)) 
     # import pdb; pdb.set_trace()
@@ -75,10 +107,16 @@ def main():
     sim.create_viewer()
     color_robot([robot], [1, 0, 0])
     env = environment.FeedbackEnvironment(sim, sim)
-    plan_trajopt(env, robot, plotting=True)
-    
 
     sim.viewer.Idle()
+    rave_robot = env.sim.env.GetRobots()[0]
+    # target_pose = get_target_pose(env, robot, go_through_hole=False)
+    target_pose = get_target_pose(env, robot, go_through_hole=True)
+    traj = plan_trajopt(env, robot, target_pose, plotting=True)
+    # import pdb; pdb.set_trace()
+    env.execute_robot_trajectory(rave_robot, traj)
+
+
 
 
     # env.execute_augmented_trajectory(test_aug_traj)
--- lfd/transfer/test_trajopt.py
+++ lfd/transfer/test_trajopt.py
@@ -13,8 +13,20 @@ from lfd.transfer import transfer
 from lfd.transfer import planning
 from lfd.util import util
 
+def base_pose_to_mat(traj, z):
+    result = np.zeros((len(traj), 4, 4))
+    for i in range(len(traj)):
+        pose = traj[i]
+        x, y, rot = pose
+        q = openravepy.quatFromAxisAngle((0, 0, rot)).tolist()
+        pos = [x, y, z]
+        mat = openravepy.matrixFromPose(q + pos)
+        # import pdb; pdb.set_trace()
+        result[i,:,:] = mat
+    return result
 
-def plan_trajopt(env, robot, plotting=False):
+
+def plan_trajopt(env, robot, target_pose, plotting=False):
     """
     Trajectory transfer of demonstrations using dual decomposition incorporating feedback
     """
@@ -24,46 +36,97 @@ def plan_trajopt(env, robot, plotting=False):
 
     dim = 2
 
+    # Get openrave robot
+    rave_robot = env.sim.env.GetRobots()[0]
+    z = rave_robot.GetTransform()[2, 3]
+    import pdb; pdb.set_trace()
+
 
     ####### Figure out trajopt call ##########
     start_fixed = True
     if start_fixed:
         pass # do something
-    n_steps = demo_traj.shape[0]
-    manip_name = "base"
+
+    n_steps = 15
+    # manip_name = "base"
+    # maintain_rel = True
+    # maintain_up = True
+    # collisionfree = True
+    # init_traj = None
+    # active_bodyparts = None #??
+    # bodypart_init_dofs= {'base': target_pose[:,3][:3]} ### What should the DOF value be here???
+    # assert type(target_pose) == list
+
+    target_pose_7 = openravepy.poseFromMatrix(target_pose).tolist()
+
+    # modify angle to be within +/- pi (not needed for now)
+    # rot_i = self.robot().GetAffineDOFIndex(openravepy.DOFAffine.RotationAxis)
+    # robot_z = self.robot().GetActiveDOFValues()[rot_i]
+    # dofs[-1] = closer_ang(dofs[-1], robot_z)
+
+
+    ################### Set up trajopt call
+    ### Base motion planning must have init dofs??
+    costs = []
+    constraints = []
+    # cost_coeffs = len(init_dofs) * [50]
+    # cost_coeffs[-1] = 500
+    # cost_coeffs[-2] = 500
+    # cost_coeffs[-3] = 500
+
+    # joint velocity cost
+    joint_vel_cost = {
+        "type": "joint_vel",
+        "params": {"coeffs": [gamma/(n_steps - 1)]}
+    }
+    costs.append(joint_vel_cost)
+
+    base_link = "base"
+
+    constraints.append({
+        "type": "pose",
+        "params": {"xyz": target_pose_7[4:],
+                   "wxyz": target_pose_7[:4],
+                   "link": base_link,
+                   "pos_coeffs": [20, 20, 20],
+                   "rot_coeffs": [20, 20, 20]}
+    })
+                    
 
     request = {
         "basic_info": {
-            "n_steps": n_steps,
-            "manip_name": manip_name,
-            "start_fixed": start_fixed
+          "n_steps": n_steps,
+          "manip": "base",
+          "start_fixed": True  # i.e., DOF values at first timestep are fixed based on current robot state
         },
-        "costs": [
-        { 
-            "type": "joint_vel",
-            "params": {"coeffs": [self.gamma/(n_steps - 1)]}
-        },
-        ],
-        "constraints" : [
-        ],
+        "costs": costs,
+        "constraints": constraints,
+        "init_info":  {
+            "type": "stationary"
+        }
     }
 
     penalty_coeffs = [3000]
     dist_pen = [0.025]
-    if self.use_collision_cost:
-        ## append collision cost
-        requests["costs"].append(
-            {
-                "type": "collision",
-                "params": {
-                    "continuous" : True,
-                    "coeffs" : penalty_coeffs, 
-                    "dist_pen" : dist_pen
-                }
-            })
 
+    request['costs'] += [{
+        "type": "collision",
+        "name": "col",
+        "params": {
+          "continuous": True,
+          # "coeffs": [20],
+          "coeffs": [7000],
+          "dist_pen": [0.02]
+        }
+    }]
+
+    prob = trajoptpy.ConstructProblem(json.dumps(request), env.sim.env)
+
+    result = trajoptpy.OptimizeProblem(prob)
+    traj = result.GetTraj()
+    import pdb; pdb.set_trace()
+    total_cost = sum(cost[1] for cost in result.GetCosts())
+    traj_mat = base_pose_to_mat(traj, z)
     
-    ##### To be implemented #####
-    ############### Update Dual variables ###############
 
-
+    return traj_mat
